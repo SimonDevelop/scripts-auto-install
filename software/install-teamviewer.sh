b181660a7115eb927c975867f0f66911b63d60eb
Simon Micheneau <contact@simon-micheneau.fr>
2018-03-06 18:32:00 +0100

    Add 'apt-get install -f' for software '.deb' and set chmod 644 for all

--- software/install-teamviewer.sh
+++ software/install-teamviewer.sh
@@ -18,6 +18,7 @@ rm /tmp/teamviewer_i386.deb
 wget https://download.teamviewer.com/download/teamviewer_i386.deb -O /tmp/teamviewer_i386.deb
 sudo apt install -y libjpeg62:i386
 sudo dpkg -i /tmp/teamviewer_i386.deb
+sudo apt-get install -f -y
 
 rm /tmp/teamviewer_i386.deb
 
