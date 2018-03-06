188554323681e3bb986546868f3a6366f22496d4
SimonDevelop <contact@simon-micheneau.fr>
2018-03-06 14:33:53 +0100

    Add filezilla and elementary scratch

--- personal/install-elementary-scratch-text-editor.sh
+++ personal/install-elementary-scratch-text-editor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+#
 ##################################################################################################################
 # Written to be used on 64 bits computers
 # Author 	: 	Simon Micheneau
@@ -10,23 +10,20 @@ set -e
 #
 ##################################################################################################################
 
-# Elementary terminal
+rm -rf /tmp/elementary-scratch
 
-sh personal/install-elementary-terminal.sh
+sudo apt-get install meson libgail-3-dev libgee-0.8-dev libgtksourceview-3.0-dev libgtkspell3-3-dev libgranite-dev libpeas-dev libsoup2.4-dev libvala-0.34-dev libvte-2.91-dev libwebkit2gtk-4.0-dev libzeitgeist-2.0 valac -y
+sudo apt-get -f install
+git clone https://github.com/elementary/code.git --depth=1 /tmp/elementary-scratch
+cd /tmp/elementary-scratch
 
-#############################################################################################
+meson build --prefix=/usr
+cd build/
+ninja test
+sudo ninja install
 
-# Elementary files manager
-
-sh personal/install-elementary-files-manager.sh
-
-#############################################################################################
-
-# Elementary scratch text editor
-
-sh personal/install-elementary-scratch-text-editor.sh
-
+rm -rf /tmp/elementary-scratch
 
 echo "################################################################"
-echo "################  personal software installed  #################"
+echo "################  elementary scratch installed  ################"
 echo "################################################################"
