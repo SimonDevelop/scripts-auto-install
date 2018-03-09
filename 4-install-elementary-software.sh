8310f716db13c05d0748b6734b287ac397af28c6
Simon Micheneau <contact@simon-micheneau.fr>
2018-03-09 18:07:17 +0100

    Remove gifup and add elementary code

--- 4-install-elementary-software.sh
+++ 4-install-elementary-software.sh
@@ -9,10 +9,11 @@
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
-# elementary repository
+# elementary repository and elementary-sdk
 
 sudo apt-add-repository ppa:elementary-os/stable -y
 sudo apt update
+sudo apt-get install elementary-sdk -y
 
 ###############################################################################################
 
@@ -24,37 +25,13 @@ sudo apt-get install pantheon-files -y
 
 # Terminal
 
-rm -rf /tmp/elementary-terminal
-
-sudo apt-get install cmake intltool libgranite-dev libvte-2.91-dev valac -y
-sudo apt-get -f install
-git clone https://github.com/elementary/terminal.git --depth=1 /tmp/elementary-terminal
-cd /tmp/elementary-terminal
-mkdir build
-cd build/
-
-cmake -DCMAKE_INSTALL_PREFIX=/usr ..
-make
-sudo make install
-
-rm -rf /tmp/elementary-terminal
+sudo apt-get install io.elementary.terminal -y
 
 ###############################################################################################
 
-# Gifup
+# Code
 
-rm -rf /tmp/elementary-gifup
-
-git clone https://github.com/BharatKalluri/Gifup.git --depth=1 /tmp/elementary-gifup
-cd /tmp/elementary-gifup
-mkdir build
-cd build/
-
-cmake -DCMAKE_INSTALL_PREFIX=/usr ../
-make
-sudo make install
-
-rm -rf /tmp/elementary-gifup
+sudo apt-get install io.elementary.code -y
 
 echo "################################################################"
 echo "###############  elementary software installed  ################"
